8b3b21a9362b61073fa36f7c2b09321027f2e3df
daijia <daijia@msn.com>
2017-08-02 21:50:53 +0800

    update weather component

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="weather-wrap">
-        <div id="weather">
+        <div id="weather" v-if="weather">
             <h2><img id="weather-icon" :src="imgUrl" width="101" height="101"></img> {{weather.now.tmp}}&deg;</h2>
             <div id="region">{{weather.basic.city}}</div>
             <div id="indexes">
@@ -9,7 +9,7 @@
                 <span>相对湿度：{{weather.now.hum}}%</span>
                 <span>气压：{{weather.now.pres}}hPa</span>
                 <span>空气质量：{{weather.aqi.city.qlty}} ({{weather.aqi.city.aqi}})</span>
-                <span>PM2.5：{{weather.aqi.city.pm25}}μg/m<sup>3</sup></span>
+                <span>PM2.5：{{weather.aqi.city.pm25}}</span>
                 <span v-if="room_temp>0">室内温度：°</span>
                 <span v-if="room_hum>0">室内湿度：%</span>
             </div>
@@ -24,15 +24,13 @@
             return {
                 weather: '',
                 imgUrl: '',
-                forecast_day0: '',
                 room_temp: 0,
                 room_hum: 0
-
             }
         },
         mounted () {
             this.updateWeather();
-            setInterval(this.updateNews, 60000)
+            setInterval(this.updateWeather, 60000)
         },
         methods: {
             updateWeather () {
@@ -43,8 +41,7 @@
                 }).then(function(json){
                     self.weather = json.HeWeather5[0];
                     self.imgUrl = './src/assets/image/weather/'+self.weather.now.cond.code+'.png';
-                    self.forecast_day0 = self.weather.daily_forecast[0].cond.txt_d == self.weather.daily_forecast[0].cond.txt_n ? self.weather.daily_forecast[0].cond.txt_d : self.weather.daily_forecast[0].cond.txt_d+'转'+self.weather.daily_forecast[0].cond.txt_n;
-                    localStorage.setItem("weather", self.weather);
+                    localStorage.setItem("weather", JSON.stringify(self.weather));
                     //console.log(json)
                 }).catch(function(error) { console.log(error); });
 
@@ -54,6 +51,18 @@
 </script>
 
 <style>
+
+    #weather-icon {
+        display: block;
+        float: left;
+        margin-left: 120px;
+        margin-right: 18px;
+        margin-top: 8px;
+    }
+    #weather, #temp {
+        text-align: right;
+        /*margin-bottom: 20px;*/
+    }
     #indexes span {
         width: 180px;
         float: right;
