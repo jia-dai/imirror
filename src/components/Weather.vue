8ad28cd62c94e5797bac596e9686151c939a92c0
daijia <daijia@msn.com>
2017-08-06 18:31:05 +0800

    add room temp&hum

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -6,19 +6,10 @@
             <div id="indexes">
                 <div><span>体感温度</span><br>{{weather.now.fl}}&deg;</div>
                 <div><span>相对湿度</span><br>{{weather.now.hum}}<small>%</small></div>
-                <div><span>空气质量</span><br>{{weather.aqi.city.qlty}}</div>
                 <div><span>PM2.5</span><br>{{weather.aqi.city.pm25}}</div>
-                <!--
-                <span>体感温度：{{weather.now.fl}}&deg;</span>
-                <span>能见度：{{weather.now.vis}}km</span>
-                <span>相对湿度：{{weather.now.hum}}%</span>
-                <span>气压：{{weather.now.pres}}hPa</span>
-                <span>空气质量：{{weather.aqi.city.qlty}} ({{weather.aqi.city.aqi}})</span>
-                <span>PM2.5：{{weather.aqi.city.pm25}}</span>
-                <span v-if="room_temp>0">室内温度：°</span>
-                <span v-if="room_hum>0">室内湿度：%</span>
-                -->
+                <div><span>空气质量</span><br>{{weather.aqi.city.qlty}}</div>
             </div>
+            <div id="temp" v-if="room_temp">室内温度：{{room_temp}}&deg; 室内湿度：{{room_hum}}<small>%</small></div>
         </div>
     </div>
 </template>
@@ -30,17 +21,18 @@
             return {
                 weather: '',
                 imgUrl: '',
-                room_temp: 0,
+                room_temp: 30,
                 room_hum: 0
             }
         },
         mounted () {
             this.updateWeather();
-            setInterval(this.updateWeather, 60000)
+            setInterval(this.updateWeather, 100000)
         },
         methods: {
             updateWeather () {
                 let api = 'https://free-api.heweather.com/v5/weather?city=%E6%B7%B1%E5%9C%B3&key=aa6b2b92808c4dd7aeb560d2b75aaf11';
+                let dht22 = "/static/dht22.json";
                 let self = this;
                 fetch(api).then(function(res){
                     return res.json();
@@ -50,6 +42,13 @@
                     localStorage.setItem("weather", JSON.stringify(self.weather));
                     //console.log(json)
                 }).catch(function(error) { console.log(error); });
+                fetch(dht22).then(function(res){
+                    return res.json();
+                }).then(function(json){
+                    self.room_temp = json.temp;
+                    self.room_hum = json.hum;
+                    console.log(json)
+                }).catch(function(error) { console.log(error); });
 
             }
         }
@@ -57,7 +56,21 @@
 </script>
 
 <style>
+    #weather h2 {
+        margin: 0 0 3px;
+        color: #F8F8FF;
+        font-size: 80px;
+        font-weight: 300;
+        text-align: right;
+    }
 
+    #weather div, #temp {
+        line-height: 30px;
+        /*margin-right: 100px;
+        font-family: 'Open Sans', sans-serif;*/
+        font-size: 1.5em;
+        margin-top: 6px;
+    }
     #weather-icon {
         display: block;
         float: left;
@@ -70,13 +83,14 @@
         /*margin-bottom: 20px;*/
     }
     #indexes div {
-        width: 99px;
-        float: right;
-        border-left: #ccc 1px solid;
+        width: 96px;
+        /*float: right;*/
+        display: inline-block;
+        border-right: #ccc 1px solid;
         text-align: center;
     }
     #indexes div:last-child {
-        border-left: none;
+        border-right: none;
     }
     #indexes div span {
         font-size: 0.66em;
